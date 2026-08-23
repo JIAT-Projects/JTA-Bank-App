bdf6d5e79bba8b6150924c3cdce8b2a7d5a854ee
Lakkhana Sudham Kalutara <141295595+LakkhanaSudhamKalutara@users.noreply.github.com>
2026-08-23 19:55:03 +0530

    Add Register servlet

---
+++ src/main/java/lk/jiat/bcd/jta/bank/servlet/Register.java
@@ -0,0 +1,45 @@
+package lk.jiat.bcd.jta.bank.servlet;
+
+import jakarta.ejb.EJB;
+import jakarta.ejb.EJBException;
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.bcd.jta.bank.ejb.remote.RegisterService;
+import lk.jiat.bcd.jta.bank.exception.DuplicateEmailException;
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+
+@WebServlet("/register")
+public class Register extends HttpServlet {
+
+    @EJB
+    private RegisterService registerService;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        try {
+
+            registerService.registerUser(name,email,password);
+            req.setAttribute("message", "User registered successfully.");
+
+            req.getRequestDispatcher("/login.jsp").forward(req, resp);
+        } catch (DuplicateEmailException e) {
+            req.setAttribute("error", e.getMessage());
+            req.getRequestDispatcher("/register.jsp").forward(req, resp);
+
+
+        }
+
+
+
+    }
+}
