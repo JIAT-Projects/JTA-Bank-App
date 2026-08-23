2455a13073189d21cb6948a86c3c77b1253711e4
Lakkhana Sudham Kalutara <141295595+LakkhanaSudhamKalutara@users.noreply.github.com>
2026-08-23 19:55:03 +0530

    Add DuplicateEmailException

---
+++ src/main/java/lk/jiat/bcd/jta/bank/exception/DuplicateEmailException.java
@@ -0,0 +1,13 @@
+package lk.jiat.bcd.jta.bank.exception;
+
+
+import jakarta.ejb.ApplicationException;
+
+@ApplicationException(rollback = true)
+public class DuplicateEmailException extends Exception{
+
+    public DuplicateEmailException(String email) {
+        super("An account already exists with the email "+ email);
+    }
+
+}
