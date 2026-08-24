6d5976ead6a8b39a8bf6a3155e7d1e24d05e9427
Lakkhana Sudham Kalutara <141295595+LakkhanaSudhamKalutara@users.noreply.github.com>
2026-08-24 19:23:59 +0530

    Update AccountService remote interface

--- src/main/java/lk/jiat/bcd/jta/bank/ejb/remote/AccountService.java
+++ src/main/java/lk/jiat/bcd/jta/bank/ejb/remote/AccountService.java
@@ -1,16 +1,28 @@
 package lk.jiat.bcd.jta.bank.ejb.remote;
 
+import jakarta.ejb.Local;
+import lk.jiat.bcd.jta.bank.entity.Account;
 import lk.jiat.bcd.jta.bank.entity.AccountType;
+import lk.jiat.bcd.jta.bank.exception.AccountNotFoundException;
 import lk.jiat.bcd.jta.bank.exception.InsufficientFundsException;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+@Local
 public interface AccountService {
-    void createAccount(String email, AccountType type, BigDecimal openingBalance );
 
     void creditToAccount(String accountNo,BigDecimal amount);
     void debitToAccount(String accountNo,BigDecimal amount) throws InsufficientFundsException;
 
+    Account findByAccountNo(String accountNo) throws AccountNotFoundException;
+
+    List<Account> findAccountsByUserEmail(String email);
+
+
+    Account createAccount(String email, AccountType type, BigDecimal openingBalance );
+
+    String generateAccountNumber(AccountType type);
 
 
 }
